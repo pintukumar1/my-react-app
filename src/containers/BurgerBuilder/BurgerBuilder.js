d3128921d01214a316b8e86345d2bcb579087e2d
pintukumar1 <kumarpintu1713122@gmail.com>
2020-09-13 09:48:16 +0530

    first commit

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,6 +31,7 @@ class BurgerBuilder extends Component {
     };
 
     componentDidMount() {
+        console.log(this.props)
         axios.get('https://react-my-burger-6cecd.firebaseio.com/ingredients.json')
         .then(response => {
             this.setState({ingredients: response.data});
@@ -91,28 +92,16 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You Continue!!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Pintu Kumar',
-                address: {
-                    street: 'Tikhampur',
-                    zipCode: '277209',
-                    country: 'India'
-                },
-                email: 'kumarpintu1713122@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-         axios.post('./orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch( error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        const queryParams= [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     };
 
     render () {
---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,72 @@
+import React , { Component } from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.css';
+import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
+
+class ContactData extends Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        },
+        loading: false
+    };
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({loading: true});
+         const order = {
+             ingredients: this.props.ingredients,
+             price: this.props.price,
+             customer: {
+                 name: 'Pintu Kumar',
+                 address: {
+                     street: 'Tikhampur',
+                     zipCode: '277209',
+                     country: 'India'
+                 },
+                 email: 'kumarpintu1713122@gmail.com'
+             },
+             deliveryMethod: 'fastest'
+         };
+          axios.post('./orders.json', order)
+             .then(response => {
+                 this.setState({loading: false});
+                 this.props.history.push('/');
+             })
+             .catch( error => {
+                 this.setState({loading: false});
+             });
+
+    };
+
+    render() {
+        let form = (
+            <form>
+                <Input inputtype="input" type="text" name="name" placeholder="Your Name"/>
+                <Input inputtype="input" type="email" name="email" placeholder="Your Mail"/>
+                <Input inputtype="input" type="text" name="street" placeholder="Street"/>
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code"/>
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner/>;
+        }
+        return (
+            <div className={classes.ContactData}>
+            <h4>Enter your Contact Data</h4>
+                {form}
+            </div>
+        );
+    }
+
+}
+
+
+export default ContactData;
